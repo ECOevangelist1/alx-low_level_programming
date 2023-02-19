509ea8829e0f88eb12b4824ba62f628472553040
Samuel Austin <obeghesamuel@gmail.com>
2023-02-19 15:24:44 -0800

    Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main function entry
+ *
+ * print letter of the alphabet
+ * Return: int(0)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letetr++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
