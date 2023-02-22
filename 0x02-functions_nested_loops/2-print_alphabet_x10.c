e3bfbdab319893c92aad397c847db951b83aab73
Samuel Austin <obeghesamuel@gmail.com>
2023-02-21 18:11:45 -0800

    alphabet 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * print_alphabet_10x - Funtion to print alphabets
+ *
+ * Return: int(0)
+ */
+void print_alphabet_10x(void)
+{
+	char letter;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			putchar(letter);
+		}
+		putchat('\n')
+	}
+}
