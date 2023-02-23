fe8364dc6b408c444f717098ca44630e61c2b595
Samuel Austin <obeghesamuel@gmail.com>
2023-02-22 16:04:28 -0800

    Signs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 /**
  * _abs-prints the absolute value of an integer.
  * @a:the integer to compute its absolute value.
- * Return:returns the abs value.
+ * Return: returns the abs value.
  */
 
 int _abs(int a)
