a7057a1f8a53f5afe0b9563c4f8156b3b76ccea2
Samuel Austin <obeghesamuel@gmail.com>
2023-02-22 17:06:02 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,16 +2,16 @@
 /**
  * print_sign - a function that prints the sign of a number
  * @n: number input
- * Return: 1 prints '+' if r > 0, 0 prints '0' if r = 0, -1 prints '-' if r < 0
+ * Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n < 0
  */
-int print_sign(n)
+int print_sign(int n)
 {
-	if (r > 0)
+	if (n > 0)
 	{
 		_putchar ('+');
 		return (1);
 	}
-	else if (r == 0)
+	else if (n == 0)
 	{
 		_putchar ('0');
 		return (0);
