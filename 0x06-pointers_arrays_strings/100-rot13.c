615767c47a0904c048de05a154ce9ea7fafd4152
Samuel Austin <obeghesamuel@gmail.com>
2023-03-03 15:03:52 -0800

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,7 +10,7 @@ char *rot13(char *s)
 {
 	int i, j:
 		char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRATUVWXYZABCDEFGHIJKLM";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRATUVWXYZABCDEFGHIJKLM";
 
 		for (i = 0; *(s + i); i++)
 		{
