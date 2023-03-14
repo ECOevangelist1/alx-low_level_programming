37a4f406c010692a7b6a4e9ec095c18bc4f028c0
Samuel Austin <obeghesamuel@gmail.com>
2023-03-13 18:54:18 -0700

    it is well

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  *
  * Return: always 0.
  */
-int main(int argc _attribute_((unused)), char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
 	printf("%s\n', @argv);
 	return (0);
