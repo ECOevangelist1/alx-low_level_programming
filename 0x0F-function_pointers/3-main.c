234adf19398695e4b55c3d7f090161bca26e648d
Samuel Austin <obeghesamuel@gmail.com>
2023-03-22 17:56:02 -0700

    God is great

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,7 @@
 #include "3-calc.h"
 
 /**
- * main - check the code for holberton school students.
+ * main - check the code for Holberton School students.
  * @argc: argument count.
  * @argv: argument vector.
  *
@@ -13,6 +13,12 @@ int main(int argc, char *argv[])
 	int (*operation)(int, int);
 
 	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(98);
