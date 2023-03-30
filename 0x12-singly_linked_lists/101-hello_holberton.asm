94c8ca86e69609f1627824cb4b1b318684f1f3a6
Samuel Austin <obeghesamuel@gmail.com>
2023-03-30 16:43:19 -0700

    Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,18 @@
-section .data
-	fmt db  "%s", 10, 0
-	msg db  "Hello, Holberton", 0
+extern printf
+
+	section .data
+msg: 		db "Hello, Holberton", 0
+fmt: 		db "%s", 10, 0
 
 	section .text
-	extern printf
-	global main
 
+	global main
 main:
 	push	rbp
-	mov	rsi, msg
-	mov	rdi, fmt
-	mov	rax, 0
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0
 	call	printf
 	pop	rbp
-	mov	rax, 0
+	mov	rax,0
 	ret
