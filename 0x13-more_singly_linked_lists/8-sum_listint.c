85fe88e062bbbfd815edff2a2ccad724e04a0957
Samuel Austin <obeghesamuel@gmail.com>
2023-12-11 16:07:03 -0800

    Sum List

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+/*
+ * File: 8-sum_listint.c
+ * Auth: Samuel Austin
+ */
+#include "lists.h"
+
+/**
+ * sum_listint - calculates the sum of all the data in a listint_t list
+ * @head: first node in the linked list
+ *
+ * Return: resulting sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+	sum += temp->n;
+	temp = temp->next;
+	}
+	return (sum);
+}
